788a19e31628bb8314e4cde680604d0725a027c9
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2018-12-21 13:58:05 +1300

    Cleaned up delete recipe api

--- src/controller/Recipe.js
+++ src/controller/Recipe.js
@@ -141,14 +141,14 @@ const Recipe = {
 
 		try {
 			const {rows} = await db.query(findOneQuery, [req.params.id]);
-
 			if(!rows[0]){
+				
 				return res.status(404).send({
 					message: "No recipe could be found"
 				})
 			}
 
-			const response = await db.query(deleteQuery, [req.params.id]);
+			await db.query(deleteQuery, [req.params.id]);
 
 			return res.status(201).send({
 				message: "Recipe deleted",
@@ -160,10 +160,6 @@ const Recipe = {
 		} catch (err) {
 			return res.status(400).send(err);
 		}
-
-		return res.status(501).send({
-			message: 'Not implemented yet'
-		})
 	}
 }
 
